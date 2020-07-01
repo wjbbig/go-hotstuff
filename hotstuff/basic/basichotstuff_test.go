f2b3cc6bd272f5ce48729b362b6e31abea0b1e63
wjbbig <JiabinWu1998@outlook.com>
2020-07-01 13:59:08 +0800

    update basic network

---
+++ hotstuff/basic/basichotstuff_test.go
@@ -0,0 +1,21 @@
+package basic
+
+import (
+	pb "go-hotstuff/proto"
+	"testing"
+	"time"
+)
+
+func TestBasicHotStuff_ReceiveMsg(t *testing.T) {
+	stuff := NewBasicHotStuff(1)
+	request := &pb.Msg_Request{Request:&pb.Request{
+		Cmd:           "1+2",
+		ClientAddress: "localhost:9999",
+	}}
+	msg := &pb.Msg{Payload:request}
+	stuff.MsgEntrance <- msg
+	stuff.MsgEntrance <- msg
+	stuff.MsgEntrance <- msg
+	defer stuff.Close()
+	time.Sleep(time.Second*2)
+}
